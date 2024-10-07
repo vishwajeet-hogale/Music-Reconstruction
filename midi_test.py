ff94077482dfa6100c3e6d19e14566ac2fdc7df8
Vishwajeet Hogale <vishwajeethogale@Vishwajeets-MacBook-Air.local>
2024-10-07 13:44:52 -0400

    Gene Info

--- midi_test.py
+++ midi_test.py
@@ -1,33 +1,51 @@
 import mido
 
-def extract_music_gene(midi_file_path):
+import mido
+
+def extract_music_gene_with_instruments(midi_file_path):
     midi_file = mido.MidiFile(midi_file_path)
     gene = []
 
+    # Dictionary to keep track of the current instrument (program) for each channel
+    channel_instruments = {}
+
     for track in midi_file.tracks:
-        time = 0  # Keep track of accumulated time
+        time = 0  # Accumulated time for note timing
         for message in track:
-            # Track the time as delta times accumulate
             time += message.time
-            if message.type == 'note_on' and message.velocity > 0:
-                # Capture note (pitch), start time, velocity, and duration info
+
+            if message.type == 'program_change':
+                # Update the instrument for the channel
+                channel_instruments[message.channel] = message.program
+
+            elif message.type == 'note_on' and message.velocity > 0:
+                # Extract note info
                 pitch = message.note
                 velocity = message.velocity
+                channel = message.channel
+                instrument = channel_instruments.get(channel, 0)  # Default to 0 if no program change
 
                 # Find duration by looking for the corresponding note_off
                 duration = 0
                 for msg in track[track.index(message)+1:]:
                     duration += msg.time
-                    if msg.type == 'note_off' and msg.note == pitch:
+                    if msg.type == 'note_off' and msg.note == pitch and msg.channel == channel:
                         break
 
-                # Append this information as part of the gene
-                gene.append((pitch, time, velocity, duration))
+                # Append pitch, time, velocity, duration, and instrument information
+                gene.append((pitch, time, velocity, duration, instrument))
 
     return gene
 
 # Example usage
-gene = extract_music_gene("./piratesofc.mid")
+gene = extract_music_gene_with_instruments("./piratesofc.mid")
+print("Extracted Gene for Music Reconstruction (with Instruments):")
+for note_info in gene:
+    print(note_info)
+
+
+# Example usage
+gene = extract_music_gene_with_instruments("./piratesofc.mid")
 print("Extracted Gene for Music Reconstruction:")
 for note_info in gene:
     print(note_info)
